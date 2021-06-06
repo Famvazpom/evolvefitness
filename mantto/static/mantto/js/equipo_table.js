0cb47d66478839362ccbe852f8822feee3818a16
Noe Vazquez P <famvazpom@gmail.com>
2021-06-05 19:09:55 -0500

    Busqueda por columnas en lista de equipos

--- mantto/static/mantto/js/equipo_table.js
+++ mantto/static/mantto/js/equipo_table.js
@@ -1,3 +1,17 @@
 $(document).ready( function () {
-    $("#equipoTable").DataTable();
+    $('#equipoTable thead tr').clone(true).appendTo( '#equipoTable thead' );
+    $('#equipoTable thead tr:eq(1) th').each( function (i) {
+        var title = $(this).text();
+        $(this).html( '<input type="text" placeholder="Buscar por '+title+'" />' );
+ 
+        $( 'input', this ).on( 'keyup change', function () {
+            if ( table.column(i).search() !== this.value ) {
+                table
+                    .column(i)
+                    .search( this.value )
+                    .draw();
+            }
+        } );
+    } );
+    var table = $("#equipoTable").DataTable();
 } );
