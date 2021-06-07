a3fdd8d571a2b828c405af1ff8138585310ec036
Noe Vazquez P <famvazpom@gmail.com>
2021-06-07 18:54:03 -0500

    Eliminar filtros de lista de equipos

--- mantto/static/mantto/js/equipo_table.js
+++ mantto/static/mantto/js/equipo_table.js
@@ -1,17 +1,4 @@
 $(document).ready( function () {
-    $('#equipoTable thead tr').clone(true).appendTo( '#equipoTable thead' );
-    $('#equipoTable thead tr:eq(1) th').each( function (i) {
-        var title = $(this).text();
-        $(this).html( '<input type="text" placeholder="Buscar por '+title+'" />' );
- 
-        $( 'input', this ).on( 'keyup change', function () {
-            if ( table.column(i).search() !== this.value ) {
-                table
-                    .column(i)
-                    .search( this.value )
-                    .draw();
-            }
-        } );
-    } );
+
     var table = $("#equipoTable").DataTable();
 } );
