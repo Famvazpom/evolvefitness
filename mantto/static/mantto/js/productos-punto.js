42b3fbcae3a1fc4da2eaa6da26f641ea7e2fb723
Noe Vazquez P <famvazpom@gmail.com>
2021-07-08 18:09:22 -0500

    Cambio

--- mantto/static/mantto/js/productos-punto.js
+++ mantto/static/mantto/js/productos-punto.js
@@ -170,7 +170,6 @@ function Cobrar()
         data: data,
         type: 'POST',
         success:function(json){
-            alert('Venta Exitosa');
             getData();
             UpdateTotal();
         },
